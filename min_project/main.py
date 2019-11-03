7c105d36fbe3a2b32c62f32ecc2bcb0d00ebdbf3
Mokshith Pyla <mokshithpyla@gmail.com>
2019-11-03 14:41:52 +0530

    add titles, comments remove junk

--- min_project/main.py
+++ min_project/main.py
@@ -1,87 +1,63 @@
 # main.py
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-sid = SentimentIntensityAnalyzer()
 from app import app
-# from db_setup import init_db, db_session
 
-# import flask_whooshalchemy as wa
-from forms import MusicSearchForm
-from flask import flash, render_template, request, redirect
+from flask import render_template, request
 
 from sentiment import predictMood
 from app import songsdata
 import sys
 import sqlite3 as lite
 
-
-def redirect_url(default='index'):
-    return request.referrer
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # try:
-    # con = lite.connect(app.config['DATABASE1'])
-    # cur = con.cursor()
-    # cur.execute('SELECT * from "songdata_compressed" ')
-    # data = cur.fetchall()
-    search = MusicSearchForm(request.form)
-    # print(d)
-    return render_template('index.html', form=search, list_of_songs=songsdata)
-    # except:
-    #     print("Error {}:")
-    #     sys.exit(1)
-    # finally:
-    #     if con:
-    #         con.close()
+    """
+        Renders Home Page
+    """
+    return render_template('index.html', list_of_songs=songsdata)
+
 
 @app.route('/search_results', methods=['GET', 'POST'])
 def search_results():
-    form = MusicSearchForm(request.form)
-    # if request.form['search_btn'] == 'Search':
+    """
+        Renders Search Results
+    """
     print('we are here')
     try:
         con = lite.connect(app.config['DATABASE1'])
         cur = con.cursor()
         search_word = request.form['song_name']
         print(search_word)
-        cur.execute('SELECT "text","artist","song" from "songdata_compressed" where upper("song") is upper("'+search_word+'")')
+        cur.execute('SELECT "text","artist","song" from "songdata_compressed" '
+                    'where upper("song") is upper("'+search_word+'")')
         data = cur.fetchall()
         print(data)
-        if data == []:
+        if len(data) == 0:
             print('yes')
-            return render_template('not_found.html', form=form, song=search_word)
-        # if not data:
-        #     cur.execute('SELECT * from "songdata_compressed"')
-        #     data = cur.fetchall()
-        # print(data)
+            return render_template('not_found.html', song=search_word)
+
     except:
         print("Error {}:")
         sys.exit(1)
     finally:
         if con:
             con.close()
-    return render_template('search_results.html', form=form, list_of_songs=data, songtitle=search_word)
-    # else:
-    #     songtitle = ''
-    #     lyrics = request.form['lyrics']
-    #     print('here', lyrics)
-    #     pred, font_color, artistname, lyrics = predictMood('','', lyrics)
-    #     return render_template('/results.html', form=form, songtitle=songtitle, font_color=font_color,
-    #                            pred=pred, artistname=artistname, lyrics=lyrics)
-    #
+    return render_template('search_results.html', list_of_songs=data, songtitle=search_word)
+
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
+    """
+    Renders Sentiment Analysis
+
+    """
     print('okay')
-    form = MusicSearchForm(request.form)
     if 'search_btn' in request.form:
         songtitle = ''
         lyrics = request.form['lyrics']
         pred, font_color, artistname, lyrics, chart_values = predictMood('', '', lyrics)
         print(pred)
-        return render_template('results.html', form=form, songtitle=songtitle, font_color=font_color,
-        pred=pred, artistname=artistname, lyrics=lyrics, chart_values=chart_values)
+        return render_template('results.html', songtitle='Unknown', font_color=font_color,
+        pred=pred, artistname='Unknown', lyrics=lyrics, chart_values=chart_values)
     else:
         print('finding song')
         print(request.form)
@@ -95,7 +71,7 @@ def results():
         artistname = artistname.strip()
         print(songtitle, artistname)
         pred, font_color, artistname, lyrics, chart_values = predictMood(songtitle, artistname, '')
-        return render_template('results.html', form=form, songtitle=songtitle, font_color=font_color,
+        return render_template('results.html', songtitle=songtitle, font_color=font_color,
                                pred=pred, artistname=artistname, lyrics=lyrics, chart_values=chart_values)
 
 
--- min_project/sentiment.py
+++ min_project/sentiment.py
@@ -5,18 +5,14 @@ import os
 import sqlite3 as lite
 import sys
 from app import app
-import matplotlib.pyplot as plt
 
 my_dir = os.path.dirname(__file__)
-
 DATABASE1 = os.path.join(my_dir, 'finalest.sqlite3')
-
-
 app.config.from_object(__name__)
 
 con = None
 def predictMood(songtitle, artistname, lyrics):
-    # try:
+    try:
         con = lite.connect(app.config['DATABASE1'])
         cur = con.cursor()
         by_song = False
@@ -27,19 +23,17 @@ def predictMood(songtitle, artistname, lyrics):
             by_song = True
         else:
             lyrics = lyrics.split('\n')
-        # print(data[0][0])
         print(lyrics)
         num_positive = 0
         num_negative = 0
         num_neutral = 0
         sid = SentimentIntensityAnalyzer()
-        # for i in range(len(data)):
         for sentence in lyrics:
             if sentence == ' ':
                 continue
             filtered_words =[]
             for each in sentence.split():
-                if each not in stopwords.words('english') and len(each) > 1 and each not in ['na','la']:
+                if each not in stopwords.words('english') and len(each) > 1 and each not in ['na', 'la']:
                     filtered_words.append(each)
                 else:
                     print(each)
@@ -50,6 +44,7 @@ def predictMood(songtitle, artistname, lyrics):
                 num_positive += 1
             elif comp > -0.25 and comp < 0.5:
                 num_neutral += 1
+            # Negativity is denoted by comp in range[-inf, -0.25]
             else:
                 num_negative += 1
         num_total = num_negative + num_neutral + num_positive
@@ -59,90 +54,23 @@ def predictMood(songtitle, artistname, lyrics):
         lyrics = '\n'.join([each for each in lyrics])
         print(lyrics)
         if percent_negative > percent_positive:
-            print('The song is sad', percent_negative)
-            print('+ve',percent_positive)
-            print('neutral',percent_neutral)
-            # print('The song '+data[0][2]+' is sad')
             font_color = 'red'
             pred = 'sad/negative'
         elif percent_negative == percent_positive:
-            font_color = 'blue'
+            font_color = 'orange'
             pred = 'mixed/neutral'
         else:
-            print('The song is happy', percent_positive)
-            print('-ve',percent_negative)
-            print(percent_neutral)
-            # print('The song '+data[0][2]+' is happy')
-            font_color = 'green'
+            font_color = 'blue'
             pred = 'happy/positive'
         if by_song:
             return pred, font_color, data[0][1], lyrics, [num_positive, num_negative, num_neutral]
         else:
             return pred, font_color, '', lyrics, [num_positive, num_negative, num_neutral]
-    # except:
-    #
-    #     print("Error {}:")
-    #     sys.exit(1)
-    #
-    # finally:
-    #     if con:
-    #         con.close()
-# def predictMood(songtitle, lyrics):
-#     try:
-#         con = lite.connect(app.config['DATABASE1'])
-#         cur = con.cursor()
-#         by_song = False
-#         if len(songtitle) != 0 and len(lyrics) == 0:
-#             cur.execute('SELECT "text","artist","song" from "songdata_compressed" where "song" is "'+songtitle+'" and ')
-#             data = cur.fetchall()
-#             lyrics = data[0][0].split('\n')
-#             by_song = True
-#         else:
-#             lyrics = lyrics.split('\n')
-#         # print(data[0][0])
-#         print(lyrics)
-#         num_positive = 0
-#         num_negative = 0
-#         num_neutral = 0
-#         sid = SentimentIntensityAnalyzer()
-#         # for i in range(len(data)):
-#         for sentence in lyrics:
-#             if sentence == ' ':
-#                 continue
-#             comp = sid.polarity_scores(sentence)
-#             comp = comp['compound']
-#             if comp >= 0.5:
-#                 num_positive += 1
-#             elif comp > -0.5 and comp < 0.5:
-#                 num_neutral += 1
-#             else:
-#                 num_negative += 1
-#         num_total = num_negative + num_neutral + num_positive
-#         percent_negative = (num_negative / float(num_total)) * 100
-#         percent_neutral = (num_neutral / float(num_total)) * 100
-#         percent_positive = (num_positive / float(num_total)) * 100
-#         lyrics = '\n'.join([each for each in lyrics])
-#         print(lyrics)
-#         if percent_negative > percent_positive:
-#             print('The song is sad', percent_negative)
-#             # print('The song '+data[0][2]+' is sad')
-#             font_color = 'red'
-#             pred = 'sad/negative'
-#
-#         else:
-#             print('The song is happy', percent_positive)
-#             # print('The song '+data[0][2]+' is happy')
-#             font_color = 'green'
-#             pred = 'happy/positive'
-#         if by_song:
-#             return pred, font_color, data[0][1], lyrics
-#         else:
-#             return pred, font_color, '', lyrics
-#     except:
-#
-#         print("Error {}:")
-#         sys.exit(1)
-#
-#     finally:
-#         if con:
-#             con.close()
+    except:
+
+        print("Error {}:")
+        sys.exit(1)
+
+    finally:
+        if con:
+            con.close()
